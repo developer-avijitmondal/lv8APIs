3b06b365c3f84d71a638d155dfa050bb9fdf0a60
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-11-19 23:01:54 +0530

    sessions

---
+++ database/migrations/2020_11_19_172025_create_user_sessions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_id');
+            $table->string('login_time');
+            $table->string('ip_address');
+            $table->string('browser');
+            $table->string('geolocation');
+            $table->string('device');
+            $table->string('login_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_sessions');
+    }
+}
